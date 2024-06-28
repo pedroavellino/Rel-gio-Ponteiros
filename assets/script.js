d6d2faf8bffdfc07c28de0de00389f33d6055fb0
Pedro Avelino de Almeida <pedroavelino@MacBook-Air-de-Pedro-2.local>
2024-06-28 15:07:28 -0300

    Projeto base do relógio de ponteiros

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const hourHand = document.querySelector(".hand.hours")
+const minutesHand = document.querySelector(".hand.minutess")
+const secondsHand = document.querySelector(".hand.secondss")
+
+const setClock = () => {
+    const currentDate = new Date();
+}
+
